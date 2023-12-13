2df99829e741fc61f8e688f5dc39dad7d73cc40b
RokasV <vezikauskas.r@gmail.com>
2023-12-13 09:31:29 +0200

    add

--- intro.js
+++ intro.js
@@ -6,3 +6,17 @@ function sum(sk1, sk2) {
 }
 
 sum(15, 45);
+
+// parasyti funkcija kurio suskaiciuoja argumentu gauto masyvo vidurki
+// ir iskonsolina rezultata
+
+function getAvg(arr) {
+  let avg = 0;
+  arr.forEach((obj) => {
+    avg += obj / arr.length;
+  });
+  console.log(avg);
+  return avg;
+}
+
+getAvg([1, 2, 3]);
